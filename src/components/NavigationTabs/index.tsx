9a10c1d487d3d67473d8e7a242dadf464dc21774
omakasebar <omakasebar@gmail.com>
2021-02-21 17:19:42 -0600

    merge useFuse and useSortableData

--- src/components/NavigationTabs/index.tsx
+++ src/components/NavigationTabs/index.tsx
@@ -56,7 +56,7 @@ const StyledArrowLeft = styled(ArrowLeft)`
   color: ${({ theme }) => theme.text1};
 `
 
-export function SwapPoolTabs({ active }: { active: 'swap' | 'pool' }) {
+export function SwapPoolTabs({ active }: { active: 'swap' | 'pool' | 'yield' }) {
   const { t } = useTranslation()
   return (
     <Tabs style={{ marginBottom: '20px', display: 'none' }}>
@@ -65,6 +65,9 @@ export function SwapPoolTabs({ active }: { active: 'swap' | 'pool' }) {
       </StyledNavLink>
       <StyledNavLink id={`pool-nav-link`} to={'/pool'} isActive={() => active === 'pool'}>
         {t('pool')}
+      </StyledNavLink>
+      <StyledNavLink id={`yield-nav-link`} to={'/yield'} isActive={() => active === 'yield'}>
+        {'Yield'}
       </StyledNavLink>
     </Tabs>
   )
